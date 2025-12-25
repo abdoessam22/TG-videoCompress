6d4791784ded61dd52fb4c5d8ed2b6e01a08d6c9
abdoessam22 <abdo57.hy@gmail.com>
2025-12-26 00:31:04 +0200

    Update OWNER configuration to a specific username

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ try:
     BOT_TOKEN = config("8190883981:AAEHJ3UifRjF94KqIJ4IjjUO8P96YfmFXeE
 ")
     DEV = 1287276743
-    OWNER = config("OWNER")
+    OWNER = config("@Zingerooo")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
